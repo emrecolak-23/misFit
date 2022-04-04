47963aa9ea7f00ed8908bfb49eba093b63c948a1
Emre Çolak <colakkemre@gmail.com>
2022-04-04 13:59:07 +0300

    nodemon installed and server created

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const express = require('express');
 // Declare Express App
 const app = express();
 
+
+app.get('/', (req, res) => {
+
+  res.send('emre çolak');
+})
+
 // Declare Port
 const PORT = 16000;
 // Listen Server
