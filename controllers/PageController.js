02b4787f69854f4db1bdbc4d304c9e1ee32653bd
Emre Çolak <colakkemre@gmail.com>
2022-04-04 15:03:02 +0300

    all page route and controller completed and partials integrated in all pages

--- controllers/PageController.js
+++ controllers/PageController.js
@@ -3,7 +3,62 @@ exports.getHomePage = (req, res) => {
     res.status(200).render('index');
   } catch(error) {
     res.status(400).json({
-      status: 'Home page not loaded',
+      status: 'Home Page not loaded',
+      error
+    })
+  }
+}
+
+exports.getAboutPage = (req, res) => {
+  try{
+    res.status(200).render('about');
+  } catch(error) {
+    res.status(400).json({
+      status: 'About Page not loaded',
+      error
+    })
+  }
+}
+
+exports.getServicePage = (req, res) => {
+  try{
+    res.status(200).render('service');
+  } catch(error) {
+    res.status(400).json({
+      status: 'Service Page not loaded',
+      error
+    })
+  }
+}
+
+exports.getTrainerPage = (req, res) => {
+  try {
+    res.status(200).render('trainer');
+  } catch(error) {
+    res.status(400).json({
+      status: 'Trainer Page not loaded',
+      error
+    })
+  }
+}
+
+exports.getGalleryPage = (req, res) => {
+  try {
+    res.status(200).render('gallery');
+  } catch(error) { 
+    res.status(400).json({
+      status: 'Gallery Page not loaded',
+      error
+    })
+  }
+}
+
+exports.getContactPage = (req, res) => {
+  try {
+    res.status(200).render('contact')
+  } catch(error) {
+    res.status(400).json({
+      status: 'Contact Page not loaded',
       error
     })
   }
--- routes/PageRoutes.js
+++ routes/PageRoutes.js
@@ -10,4 +10,19 @@ const router = express.Router();
 // Home Page
 router.route('/').get(PageController.getHomePage);
 
+// About Page
+router.route('/about').get(PageController.getAboutPage);
+
+// Services Page
+router.route('/service').get(PageController.getServicePage);
+
+// Trainer Page
+router.route('/trainer').get(PageController.getTrainerPage);
+
+// Gallery Page
+router.route('/gallery').get(PageController.getGalleryPage);
+
+// Contact page
+router.route('/contact').get(PageController.getContactPage);
+
 module.exports = router
