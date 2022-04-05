ec2f1ff9763f15237d32a0085bc8a7fc4429926d
Emre Çolak <colakkemre@gmail.com>
2022-04-05 13:47:33 +0300

    bcrypt modules installed and integrated in User model

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 // Import Packages
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 // Create Schema
 const Schema = mongoose.Schema;
@@ -20,6 +21,15 @@ const UserSchema = new Schema({
     type: String,
     required: true
   }
+})
+
+UserSchema.pre('save', function(next){
+  const user = this;
+  bcrypt.hash(user.password, 10, function(error, hashed){
+    if(error) throw error
+    user.password = hashed
+    next();
+  })
 })
 
 // Create User Model
