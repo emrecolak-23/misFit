75450fead72b4343eeaef38ad8d6af7e57bf942b
Emre Çolak <colakkemre@gmail.com>
2022-04-05 14:32:02 +0300

    logout functionality completed

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -7,32 +7,41 @@ exports.registerUser = (req, res) => {
   try {
     const user = User.create(req.body);
     res.status(200).redirect('/login');
-  } catch(error) {
+  } catch (error) {
     res.status(400).redirect('/register');
   }
-}
+};
 
 exports.loginUser = async (req, res) => {
   try {
-    const {email,password} = req.body
+    const { email, password } = req.body;
 
-    const user = await User.findOne({email});
+    const user = await User.findOne({ email });
 
     if (user) {
-      bcrypt.compare(password,user.password, (err,same)=>{
-        if(same) {
+      bcrypt.compare(password, user.password, (err, same) => {
+        if (same) {
           req.session.userID = user._id;
           res.status(200).redirect('/gallery');
         } else {
           res.status(400).redirect('/login');
         }
-      })
+      });
     }
-
-  } catch(error) {
+  } catch (error) {
     res.status(400).json({
       status: 'Sometinhg went wrong',
-      error
+      error,
+    });
+  }
+};
+
+exports.logout = (req, res) => {
+  try {
+    req.session.destroy(()=>{
+      res.redirect('/')
     })
+  } catch(error) {
+    res.status(400).redirect('/')
   }
 }
--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -9,5 +9,6 @@ const router = express.Router();
 
 router.route('/register').post(AuthController.registerUser);
 router.route('/login').post(AuthController.loginUser);
+router.route('/logout').get(AuthController.logout);
 
 module.exports = router;
