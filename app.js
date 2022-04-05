40b3f7f4719841ea19e24a0f6020e9bf7e31b475
Emre Çolak <colakkemre@gmail.com>
2022-04-05 14:38:42 +0300

    connect-mongo installed and prevent to loss session when page reload or server restart

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 const session = require('express-session');
-
+const MongoStore = require('connect-mongo');
 // Import Routers
 const PageRouter = require('./routes/PageRoutes');
 const WorkoutRouter = require('./routes/WorkoutRoutes');
@@ -11,6 +11,21 @@ const CategoryRouter = require('./routes/CategoryRoutes');
 const UserRouter = require('./routes/UserRoutes');
 // Declare Express App
 const app = express();
+
+// DB Connect
+const dbURI =
+  'mongodb+srv://emco:emco3232@nodetuts.iuulr.mongodb.net/misFitApp?retryWrites=true&w=majority';
+mongoose
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((result) => {
+    console.log('Db connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 // Template Engine
 app.set('view engine', 'ejs');
@@ -22,11 +37,14 @@ global.userIN = null;
 app.use(express.static('public'));
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-app.use(session({
-  secret: "my-secret-value",
-  resave: false,
-  saveUninitialized: true
-}));
+app.use(
+  session({
+    secret: 'my-secret-value',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: dbURI }),
+  })
+);
 app.use('*', (req, res, next) => {
   userIN = req.session.userID;
   next();
@@ -38,25 +56,9 @@ app.use('/workout', WorkoutRouter);
 app.use('/category', CategoryRouter);
 app.use('/user', UserRouter);
 
-// DB Connect
-const dbURI =
-  "mongodb+srv://emco:emco3232@nodetuts.iuulr.mongodb.net/misFitApp?retryWrites=true&w=majority";
-mongoose
-  .connect(dbURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then((result) => {
-    console.log("Db connected");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
-
 // Declare Port
 const PORT = 16000;
 // Listen Server
-app.listen(PORT, ()=> {
-  console.log(`Server listened in ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server listened in ${PORT}`);
+});
